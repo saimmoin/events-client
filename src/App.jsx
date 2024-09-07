3202c127779b33d0115f13731364f67ce0bd0b15
Saim Saqib <saimmoin64@gmail.com>
2024-09-07 12:19:22 +0500

    tailwind and shardcn-ui initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+/** @format */
+
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
+  const account = useAccount();
+  const { connectors, connect, status, error } = useConnect();
+  const { disconnect } = useDisconnect();
 
   return (
     <>
       <div>
-        <h2>Account</h2>
+        <h2 className="text-3xl font-bold underline">Account</h2>
+
+        {/* <Button className="text-3xl font-bold underline">Button</Button> */}
 
         <div>
           status: {account.status}
@@ -18,8 +23,8 @@ function App() {
           chainId: {account.chainId}
         </div>
 
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
+        {account.status === "connected" && (
+          <button className="h-10 px-6 font-semibold rounded-md bg-black text-white font-mono" type="button" onClick={() => disconnect()}>
             Disconnect
           </button>
         )}
@@ -29,6 +34,7 @@ function App() {
         <h2>Connect</h2>
         {connectors.map((connector) => (
           <button
+            className="h-10 px-6 font-semibold rounded-md bg-black text-white font-mono"
             key={connector.uid}
             onClick={() => connect({ connector })}
             type="button"
@@ -40,7 +46,7 @@ function App() {
         <div>{error?.message}</div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+/** @format */
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
